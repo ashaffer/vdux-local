4f7d87b9f481c79db3f97c31c9618d8e27358c3e
Andrew Shaffer <darawk@gmail.com>
2015-09-20 18:19:50 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+/**
+ * Imports
+ */
+
+import {createEphemeral, updateEphemeral, destroyEphemeral} from 'redux-ephemeral'
+
+/**
+ * Types
+ */
+
+const SET_STATE = 'SET_LOCAL_STATE'
+
+/**
+ * Vdux local
+ */
+
+function localize (component) {
+  if (typeof component === 'function') {
+    component = {render: compoonent}
+  }
+
+  return {
+    ...component,
+    render: props => component.render(props, setState(props)),
+    beforeMount: compose(beforeMount(component.initialState), component.beforeMount),
+    beforeUnmount: compose(beforeUnmount, component.beforeUnmount)
+  }
+}
+
+/**
+ * setState action creator
+ */
+
+function setState (props) {
+  return newState => updateEphemeral(props.key, {
+    type: SET_STATE,
+    payload: newState
+  })
+}
+
+/**
+ * Reducer
+ */
+
+function reducer (state, action) {
+  switch (action.type) {
+    case SET_STATE:
+      return {
+        ...state,
+        action.payload
+      }
+  }
+
+  return state
+}
+
+/**
+ * Hooks
+ */
+
+function beforeMount (initialState = () => {}) {
+  return props => {
+    if (!props.key) {
+      throw new Error('You cannot create a local component without a props.key')
+    }
+
+    return createEphemeral(props.key, reducer, initialState(props))
+  }
+}
+
+function beforeUnmount (props) {
+  if (!props.key) {
+    throw new Error('You cannot create a local component without a props.key')
+  }
+
+  return destroyEphemeral(props.key)
+}
+
+/**
+ * Action creator composition utility
+ */
+
+function compose (a, b) {
+  return props =>
+    b
+      ? [].concat(b(props), a(props)).filter(Boolean)
+      : a(props)
+}
+
+/**
+ * Exports
+ */
+
+export default local
---
+++ test/index.js
@@ -0,0 +1,14 @@
+/**
+ * Imports
+ */
+
+import test from 'tape'
+import vdux-local from '../src'
+
+/**
+ * Tests
+ */
+
+test('should work', () => {
+
+})
